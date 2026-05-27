72cf07748e87eae8ac53e94ca210ab221f538555
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-27 21:38:56 +0600

    Model UI Done

--- src/Models/Model/Model.jsx
+++ src/Models/Model/Model.jsx
@@ -1,14 +1,78 @@
-import "../../index.css"
+import "../../index.css";
 
 const Model = ({ model }) => {
-    console.log(model)
-    return (
-        <div>
-            <div>
-                <img src={model.image} alt={model.title} />
-            </div>
+  return (
+    <div className="bg-white border border-gray-200 rounded-[30px] overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300">
+      {/* Top Section */}
+      <div className="relative bg-[#ECECEF] h-55 flex items-center justify-center">
+        {/* Badge */}
+        <span
+          className={`
+            absolute top-5 right-5
+            text-white text-[12px]
+            font-semibold
+            px-5 py-2
+            rounded-full
+            tracking-wide
+            min-w-36.25
+            text-center
+
+            ${
+              model.status === "Popular"
+                ? "bg-red-600"
+                : model.status === "Favourite"
+                  ? "bg-orange-500"
+                  : "bg-yellow-500"
+            }
+          `}
+        >
+          {model.status === "Popular" && "🔥"}
+          {model.status === "Favourite" && "🧡"}
+          {model.status === "Most Wanted" && "⭐"} {model.status}
+        </span>
+
+        {/* Model Image */}
+        <img
+          className="w-40 h-40 object-contain"
+          src={model.image}
+          alt={model.title}
+        />
+      </div>
+
+      {/* Bottom Content */}
+      <div className="p-7">
+        {/* Title */}
+        <h1 className="text-[38px] md:text-[30px] font-bold text-[#111827] mb-3 leading-none">
+          {model.title}
+        </h1>
+
+        {/* Description */}
+        <p className="text-gray-500 text-[15px] leading-7 mb-7 min-h-[90px]">
+          {model.description}
+        </p>
+
+        {/* Price */}
+        <div className="flex items-end gap-1 mb-7">
+          <span
+            className={`text-4xl font-bold ${
+              model.price === "Free" ? "text-green-500" : "text-black"
+            }`}
+          >
+            {model.price === "Free" ? "Free" : `$${model.price}`}
+          </span>
+
+          {model.price !== "Free" && (
+            <span className="text-gray-500 text-lg mb-1">/month</span>
+          )}
         </div>
-    );
+
+        {/* Button */}
+        <button className="w-full bg-red-600 hover:bg-red-700 transition-all duration-300 text-white font-semibold text-lg py-4 rounded-2xl shadow-lg shadow-red-500/20 cursor-pointer">
+          Subscribe Now
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Model;
--- src/Models/Models.jsx
+++ src/Models/Models.jsx
@@ -5,21 +5,18 @@ const Models = ({ modelsPromise }) => {
   const models = use(modelsPromise);
   console.log(models);
   return (
-    <div className="max-w-6xl mx-auto px-6 py-10 ">
-     
+    <div className="max-w-350 mx-auto px-4 py-16">
       <div className="text-center">
-        
-        <h1 className="font-bold text-5xl text-black">
-          Choose Your AI Model
-        </h1>
-        <p className="text-xl text-gray-500">
-          
+        <h1 className="font-bold text-5xl text-black">Choose Your AI Model</h1>
+        <p className="text-xl text-gray-500 ">
           One subscription will gives you access to all frontier AI models.
         </p>
       </div>
-      {models.map(model => (
-        <Model key={model.id} model={model}></Model>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-20">
+        {models.map((model) => (
+          <Model key={model.id} model={model}></Model>
+        ))}
+      </div>
     </div>
   );
 };
